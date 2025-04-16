5da73898977acc3635842880fe296677ada5a796
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2025-04-16 11:51:01 +0900

    start

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,33 @@
 #include <Windows.h>
 
+#include "KamataEngine.h"
+
+using namespace KamataEngine;
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+	KamataEngine::Initialize(L"LE3D_12_ヒガ_ワタル");
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	while (true)
+	{
+
+		if (KamataEngine::Update())
+		{
+			break;
+		}
+
+		dxCommon->PreDraw();
+
+
+
+		dxCommon->PostDraw();
+
+	}
+
+	KamataEngine::Finalize();
+
 	return 0;
 }
