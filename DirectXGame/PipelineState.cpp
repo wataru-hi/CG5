3267a05c089b57bc1703d4cd0283c80c9eaad3c8
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2025-07-25 21:02:58 +0900

    itttanPush

--- DirectXGame/PipelineState.cpp
+++ DirectXGame/PipelineState.cpp
@@ -5,19 +5,17 @@ using namespace KamataEngine;
 
 // PipelineStateを生成する
 void PipelineState::Create(D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc) {
-	// graphicsPipelineStateDescを参照することで警告を回避
-	(void)graphicsPipelineStateDesc;
-
-	
+	// クラス内で取得するために追加
+	[[maybe_unused]] DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
 	ID3D12PipelineState* graphicsPipelineState = nullptr;
+	[[maybe_unused]] HRESULT hr = dxCommon->GetDevice()->CreateGraphicsPipelineState(&graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPipelineState));
 #ifdef _DEBUG
 
-	// クラス内で取得するために追加
-	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-	HRESULT hr = dxCommon->GetDevice()->CreateGraphicsPipelineState(&graphicsPipelineStateDesc, IID_PPV_ARGS(&graphicsPipelineState));
 	assert(SUCCEEDED(hr));
-#endif
+
+#endif // _DEBUG
+	
 	// 生成した PipelineState をとっておく
 	pipelineState_ = graphicsPipelineState;
 }
